3dc4e2f682e4e4136a21374b2ffd17e715c1637b
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-10-04 11:05:34 -0700

    styles applied

---
+++ src/components/GameBoard/GameBoard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Box from '../Box/Box';
+import './GameBoard.css';
+
+export default function GameBoard() {
+  return (
+    <div className='game-board'>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Box />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Message/Message.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Message.css';
+
+export default function Message() {
+  return (
+    <div className='game-status'>Game Status</div>
+  );
+}
